228dd4b161e76723e5d478a44720cdcce7308083
Luke Gartner <lukegartner4@gmail.com>
2023-07-20 14:01:27 -0500

    add MUI

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,11 +2,12 @@ import { HashRouter as Router, Route } from "react-router-dom";
 import "./App.css";
 import MovieList from "../MovieList/MovieList";
 import MovieDetails from "../MovieDetails/MovieDetails";
+import Header from "./Header";
 
 function App() {
   return (
     <div className="App">
-      <h1>The Movies Saga!</h1>
+      <Header />
       <Router>
         <Route path="/" exact>
           <MovieList />
